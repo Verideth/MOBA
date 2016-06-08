9e99a43503c83e185ecb93c58a2c2d8f58c672d2
Verideth <geckoman009@gmail.com>
2016-06-08 18:43:26 -0400

    Fixed it up. Why does github keep doing this lmfao

--- MOBA/Mapeditor.h
+++ MOBA/Mapeditor.h
@@ -35,79 +35,7 @@ private:
 class MapEditor
 {
 public:
-<<<<<<< HEAD
 	void OpenMapEditor();
-	sf::Vector2f Snap(sf::RenderWindow &mapEditor);
-	sf::CircleShape Color();
-=======
-	void OpenMapEditor()
-	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::F1))
-		{
-			isInEditor = true;
-
-			if (isInEditor)
-			{
-				sf::RenderWindow mapEditor(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "Battle of Hecarix Map Editor");
-
-				while (mapEditor.isOpen()) // game loop
-				{
-					sf::Event evnt; // event
-
-					std::vector<sf::RectangleShape> rect(100, sf::RectangleShape(rectShape));
-					std::vector<sf::CircleShape> circle(100, sf::CircleShape(circleShape));
-
-					Button circleButton;
-
-					while (mapEditor.pollEvent(evnt)) // check for events
-					{
-
-						if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-						{
-							for (std::vector<sf::RectangleShape>::iterator i = rect.begin(); i != rect.end(); i++)
-							{
-								i->setFillColor(sf::Color(25, 255, 25, 255));
-								i->setPosition(mouse.getPosition().x, mouse.getPosition().y);
-								i->setSize(sf::Vector2<float>(200, 500));
-
-								mapEditor.draw(*i);
-							}
-						}
-
-
-						/*if (circleButton.IsOnPress() == false)
-						{
-							isCircleButtonPressed = true;
-
-							if (isCircleButtonPressed)
-							{
-								if (sf::Mouse::Button(sf::Mouse::Left))
-								{
-									for (std::vector<sf::CircleShape>::iterator i = circle.begin(); i != circle.end(); i++)
-									{
-										i->setFillColor(sf::Color(25, 255, 25, 255));
-										i->setPosition(mouse.getPosition().x, mouse.getPosition().y);
-										i->setRadius(100);
-
-										mapEditor.draw(*i);
-									}
-								}
-							}
-						}*/
-
-						if (evnt.type == sf::Event::Closed) // if pressed exit then window close
-						{
-							mapEditor.close();
-						}
-
-						mapEditor.draw(circleButton.Draw(25, 25, 50, 50, sf::Color(255, 255, 255, 255)));
-						mapEditor.display();
-					}
-				}
-			}
-		}
-	}
->>>>>>> origin/master
 
 private:
 	MapGUI gui;
